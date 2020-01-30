0a3a23c8f8eb338d9f438dda6df1b9bd3ec34480
maisonmark <60351590+maisonmark@users.noreply.github.com>
2020-01-30 13:27:28 -0500

    secondary commit

---
+++ MyAgeIs.java
@@ -0,0 +1,14 @@
+public class MyAgeIs {
+
+	public static void main(String[] args){
+
+		int currentyear = 2020;
+		int yearOfBirth = 2002;
+
+		int age;
+
+		age = currentyear - yearOfBirth;
+
+		System.out.println(" My age is " + age);
+	}
+}
