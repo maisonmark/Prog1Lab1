26aa91decacd4b90a8b7f7194747a68f1c95ad37
maisonmark <60351590+maisonmark@users.noreply.github.com>
2020-01-30 14:01:17 -0500

    fourth commit

---
+++ Sandbox.java
@@ -0,0 +1,24 @@
+public class Sandbox {
+
+	public static void main(String[] args) {
+		// Prints "Enter Sanbox" to the terminal window
+		System.out.println("Enter Sanbox");
+		
+		int year, nextYear;
+		double distance;
+		char letter;
+		boolean state;
+
+		year = 2020;
+		nextYear = year +1;
+
+
+		System.out.println( "The current year is " + year);
+		System.out.println( "The Next year is " + nextYear);
+
+		year = 2020;
+		distance = 2.3;
+		letter = 'a';
+		state = true;
+	}
+}	
